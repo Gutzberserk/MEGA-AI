cf124d03dcae7e234d0d2c19e7b621dde77edcc1
Dilkhush Kumar <dilkhushkumar01580@gmail.com>
2025-04-13 08:57:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '917870107458';
+const defaultOwner = '3584573999082';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '917870107458' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '3584573999082' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,12 +20,12 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '917870107458' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'yUIjURwQ#FYm0YefrpcFI291T_MPQw6p71mH_6gOWSTwcnktZOB8' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '3584573999082' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
 global.prems = []
-global.allowed = ['917870107458', '']
+global.allowed = ['3584573999082', '']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
